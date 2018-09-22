134eafff1aaf2385b6f388f255061c352173c6fd
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2018-09-22 13:57:40 -0700

    Created

---
+++ lib/boxes.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class TapboxA extends StatefulWidget {
+  TapboxA({ Key key }) : super(key: key);
+
+  @override 
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Boxes')
+      ),
+      body: Center(
+        child: (
+          GestureDetector(
+            onTap: _handleTap,
+            child: Container(
+              child: Center (
+                child: Text(
+                  _active ? 'Active' : 'Inactive',
+                  style: TextStyle(fontSize: 32.0, color: Colors.white),
+                ),
+              ),
+              width: 200.0,
+              height: 200.0,
+              decoration: BoxDecoration(
+                color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+              ),
+            ),
+          )
+        ),
+      ),
+    );
+  }
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_layouts/boxes.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.red,
+      ),
+      home: HomeWidget(),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Title section.
+    var titleSection = Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          FavoriteWidget(),
+        ],
+      ),
+    );
+
+    // Function to build a a button column
+    Column buildButtonColumn(IconData icon, String label) {
+      var color = Theme.of(context).primaryColor;
+
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+    
+    // Button section.
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildButtonColumn(Icons.call, 'CALL'),
+          buildButtonColumn(Icons.near_me, 'ROUTE'),
+          buildButtonColumn(Icons.share, 'SHARE')
+        ],
+      ),
+    );
+    
+    // Text section.
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Text(
+        '''Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.''',
+        softWrap: true,
+      ),
+    );
+
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Hello world'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Image.asset(
+            'images/lake.jpg',
+            width: 600.0,
+            height: 240.0,
+            fit: BoxFit.cover
+          ),
+          titleSection,
+          buttonSection,
+          textSection,
+          Container(
+            padding: const EdgeInsets.all(32.0),
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.of(context).push(new MaterialPageRoute<void>(
+                  builder: (BuildContext context) {
+                    return new TapboxA();
+                  })
+                );
+              },
+              splashColor: Colors.blueGrey,
+              color: Theme.of(context).accentColor,
+              textColor: Colors.white,  
+              elevation: 4.0,
+              child: const Text('Next')
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    }); 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0.0),
+          child: IconButton(
+            icon: (_isFavorited
+              ? Icon(Icons.star)
+              : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18.0,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
